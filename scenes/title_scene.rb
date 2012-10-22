ac91b970a3096c3a91c663f96ceacdb24d525159
Franklin Webber <franklin.webber@gmail.com>
2012-10-21 22:01:07 -0400

    TitleScene - drawing a title

--- scenes/title_scene.rb
+++ scenes/title_scene.rb
@@ -1,3 +1,9 @@
 class TitleScene < Metro::Scene
 
+  draw :title, 'text' => 'Title Screen',
+    'x' => 20, 'y' => 20, 'z-order' => 0,
+    'x-factor' => 3, 'y-factor' => 3,
+    'color' => 0xffffffff,
+    'model' => 'metro::models::label'
+
 end
