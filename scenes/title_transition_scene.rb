4b8004eb1107fb2e43aebe943e721a7f44479313
Franklin Webber <franklin.webber@gmail.com>
2012-10-22 17:53:22 -0400

    Title Transition - player animation

--- scenes/title_transition_scene.rb
+++ scenes/title_transition_scene.rb
@@ -3,4 +3,9 @@ class TitleTransitionScene < Metro::Scene
   draw :title, model: 'metro::models::label', from: :previous_scene
   draw :player, model: 'metro::models::image', from: :previous_scene
 
+  def show
+    final_x, final_y = Metro::Game.center
+    animate actor: player, to: { x: final_x, y: final_y }, interval: 60
+  end
+
 end
