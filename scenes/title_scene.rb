a79696f0af3043410929ae0cd697fa37b03bb896
Franklin Webber <franklin.webber@gmail.com>
2012-10-22 14:36:39 -0400

    Title View - YAML format

--- scenes/title_scene.rb
+++ scenes/title_scene.rb
@@ -1,18 +1,6 @@
 class TitleScene < Metro::Scene
 
-  draw :title, 'text' => 'Title Screen',
-    'x' => 20, 'y' => 20, 'z-order' => 0,
-    'x-factor' => 3, 'y-factor' => 3,
-    'color' => 0xffffffff,
-    'model' => 'metro::models::label'
-    
-  draw :menu,  'x' => 320, 'y' => 240, 'z-order' => 0,
-    'padding' => 40,
-    'color' => 0xffffffff,
-    'highlight-color' => 0xffffff00,
-    'options' => [ 'Start Game', 'Exit Game' ],
-    'model' => 'metro::models::menu'
-
+  draws :title, :menu
 
   def start_game
     puts "Starting the Game"
@@ -21,5 +9,5 @@ class TitleScene < Metro::Scene
   def exit_game
     exit
   end
-  
+
 end
