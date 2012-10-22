89569c390870eb201550ca0289a645025c78849a
Franklin Webber <franklin.webber@gmail.com>
2012-10-22 17:42:00 -0400

    Title Transition - loading actors from previous scene

--- scenes/title_transition_scene.rb
+++ scenes/title_transition_scene.rb
@@ -1,3 +1,6 @@
 class TitleTransitionScene < Metro::Scene
 
+  draw :title, model: 'metro::models::label', from: :previous_scene
+  draw :player, model: 'metro::models::image', from: :previous_scene
+
 end
